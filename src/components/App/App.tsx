4dc15657c4596c5f7617bf0c0baa94f59194a75b
Vadim Klus <vadim178963@gmail.com>
2025-07-02 23:50:40 +0300

    addSearch

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import SearchBar from "../SearchBar/SearchBar";
+import styles from "./App.module.css";
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <SearchBar />
+    </div>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
 import "./index.css";
-import App from "./components/App/App/App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
